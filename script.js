070319cd849265e9b2c9a0b51b61a8e24d4b7828
Farel Rizky <147903975+FarelX@users.noreply.github.com>
2026-02-25 01:41:08 +0700

    first commit: Twimba Twitter Clone

---
+++ script.js
@@ -0,0 +1,167 @@
+import { tweetsData } from "./data.js";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  } else if (e.target.dataset.retweet) {
+    handleRetweetClick(e.target.dataset.retweet);
+  } else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply);
+  } else if (e.target.id === "tweet-btn") {
+    handleTweetBtnClick();
+  } else if (e.target.dataset.delete) {
+    handleDeleteBtnClick(e.target.dataset.delete);
+  }
+});
+
+function handleLikeClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+
+  if (targetTweetObj.isLiked) {
+    targetTweetObj.likes--;
+  } else {
+    targetTweetObj.likes++;
+  }
+  targetTweetObj.isLiked = !targetTweetObj.isLiked;
+  localStorage.setItem("tweets", JSON.stringify(tweetsData));
+  render();
+}
+
+function handleRetweetClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+
+  if (targetTweetObj.isRetweeted) {
+    targetTweetObj.retweets--;
+  } else {
+    targetTweetObj.retweets++;
+  }
+  targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
+  localStorage.setItem("tweets", JSON.stringify(tweetsData));
+  render();
+}
+
+function handleReplyClick(replyId) {
+  document.getElementById(`replies-${replyId}`).classList.toggle("hidden");
+}
+
+function handleTweetBtnClick() {
+  const tweetInput = document.getElementById("tweet-input");
+
+  if (tweetInput.value) {
+    tweetsData.unshift({
+      handle: `@Farel`,
+      profilePic: `images/user-avatar.jpg`,
+      likes: 0,
+      retweets: 0,
+      tweetText: tweetInput.value,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: uuidv4(),
+    });
+    localStorage.setItem("tweets", JSON.stringify(tweetsData));
+    render();
+    tweetInput.value = "";
+  }
+}
+
+function loadTweetsFromStorage() {
+  const storedTweets = JSON.parse(localStorage.getItem("tweets"));
+  if (storedTweets) {
+    tweetsData.splice(0, tweetsData.length, ...storedTweets);
+  }
+}
+
+function handleDeleteBtnClick(deleteId) {
+  confirm("Apakah Anda yakin ingin menghapus tweet ini?");
+  const targetIndex = tweetsData.findIndex(function (index) {
+    return index.uuid === deleteId;
+  });
+  tweetsData.splice(targetIndex, 1);
+  localStorage.setItem("tweets", JSON.stringify(tweetsData));
+  render();
+}
+
+function getFeedHtml() {
+  let feedHtml = ``;
+
+  tweetsData.forEach(function (tweet) {
+    let likeIconClass = "";
+
+    if (tweet.isLiked) {
+      likeIconClass = "liked";
+    }
+
+    let retweetIconClass = "";
+
+    if (tweet.isRetweeted) {
+      retweetIconClass = "retweeted";
+    }
+
+    let repliesHtml = "";
+
+    if (tweet.replies.length > 0) {
+      tweet.replies.forEach(function (reply) {
+        repliesHtml += `
+<div class="tweet-reply">
+    <div class="tweet-inner">
+        <img src="${reply.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${reply.handle}</p>
+                <p class="tweet-text">${reply.tweetText}</p>
+            </div>
+        </div>
+</div>
+`;
+      });
+    }
+
+    feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart ${likeIconClass}"
+                    data-like="${tweet.uuid}"
+                    ></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet ${retweetIconClass}"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>  
+        <i class="fa-solid fa-circle-minus" data-delete="${tweet.uuid}"></i>
+    </div>
+    <div class="hidden" id="replies-${tweet.uuid}">
+        ${repliesHtml}
+    </div>   
+</div>
+`;
+  });
+  return feedHtml;
+}
+
+function render() {
+  document.getElementById("feed").innerHTML = getFeedHtml();
+}
+loadTweetsFromStorage();
+render();
